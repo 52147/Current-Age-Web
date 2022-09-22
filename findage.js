b93479bbce3ea96accbf3dba3e45d560df4dba94
Your Name <debra4117@gmail.com>
2022-09-22 15:36:12 -0500

    update

--- findage.js
+++ findage.js
@@ -11,72 +11,47 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-
-
-
-
-
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
+// post request to get the user input, calculate it and render it back
 app.post("/", function (req, res) {
 
+    // use Date() object to generate current date, month, year and time
     var today = new Date();
-    var current_date = String(today.getDate()).padStart(2, '0');
-    var current_month = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var current_date = String(today.getDate()).padStart(2, '0'); // if string length is not 2, fill with 0
+    var current_month = String(today.getMonth() + 1).padStart(2, '0');
     var current_year = today.getFullYear();
 
-
-    var birth_date = parseFloat(req.body.birth_date);
+    // get the user input of birthday
+    var birth_date = parseFloat(req.body.birth_date); // convert string to float
 
     var birth_month = parseFloat(req.body.birth_month);
     var birth_year = parseFloat(req.body.birth_year);
 
-    // var bmi = weight / (height * height);
-
-    // var current_date = 7;
-    // var current_month = 12;
-    // var current_year = 2017;
-    // // birth dd// mm// yyyy
-    // var birth_date = 16;
-    // var birth_month = 12;
-    // var birth_year = 2009;
-
-
-
-    // if birth date is greater than current date
-    // then do not count this month and add 30
-    // to the date so as to subtract the date and
-    // get the remaining days
+    // if birth_date > current_date, add 30 to current date and minus 1 to current months
     if (birth_date > current_date) {
         current_date = current_date + month[birth_month - 1];
         current_month = current_month - 1;
     }
-
-    // if birth month exceeds current month, then do
-    // not count this year and add 12 to the month so
-    // that we can subtract and find out the difference
+    // if birth_month > current_month, minus 1 to current year and add 12 to current months
     if (birth_month > current_month) {
         current_year = current_year - 1;
         current_month = current_month + 12;
     }
 
-    // calculate date, month, year
+    // compute the current age
     var res_date = current_date - birth_date;
     var res_month = current_month - birth_month;
     var res_year = current_year - birth_year;
 
-    // print the present age
-    // document.write("Present Age<br>Years: " + (calculated_year) + " ");
-    // document.write("Months: " + calculated_month + " ");
-    // document.write("Days: " + calculated_date + " ");
-
+    // today time
     var res_today = current_month + '/' + current_date + '/' + current_year;
 
-    res.send("Today is " + res_today + ". Your present age is " + res_year + " years old and " + res_month + " months " + res_date + " days."); // .toFixed(1) : display the number have one decimal
+    // after user sumbit the input, today time and current age render back to web
+    res.send("Today is " + res_today + ". Your present age is " + res_year + " years old and " + res_month + " months " + res_date + " days.");
 })
-
 
 // start up server use app.listen()
 // listen on port 3000 and have a call back
